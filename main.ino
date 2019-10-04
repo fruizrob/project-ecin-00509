fb75bd4e926ff1a409f9c14478e10c8ba3aee69b
Tomás Vélez Núñez <43381969+txvnt@users.noreply.github.com>
2019-10-04 19:07:26 -0300

    Added EnA,EnB
    Speed changes working!

--- main.ino
+++ main.ino
@@ -1,10 +1,9 @@
 const int in1 = 6;
 const int in2 = 7;
-const int EnA = 5;
+const int EnA = 10;
 const int in3 = 8;
 const int in4 = 9;
-const int EnB = 10;
-
+const int EnB = 5;  
 
 void motors_stop();
 void motors_forward();
@@ -20,6 +19,21 @@ void setup() {
 }
 
 void loop() {
+
+  digitalWrite(EnA,HIGH);
+  digitalWrite(EnB,HIGH);
+  analogWrite(EnA,255);
+  analogWrite(EnB,255);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  delay(1000);
+  digitalWrite(EnA,LOW);
+  digitalWrite(EnB,LOW);
+  delay(1500);
+  /*
+  
   motors_stop();
   delay(1000);
   motors_right();
@@ -28,6 +42,7 @@ void loop() {
   delay(1000);
   motors_left();
   delay(1000);
+  */
 }
 
 void motors_stop() {
@@ -52,6 +67,7 @@ void motors_forward() {
 }
 
 void motors_right() {
+  analogWrite(EnB,50);
   digitalWrite(6, LOW);
   digitalWrite(7, HIGH);
   digitalWrite(8, LOW);
