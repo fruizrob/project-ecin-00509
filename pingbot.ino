a4d3e62402e12c413831945342937f2590a2acea
Felipe Ruiz <fruiz.rob@gmail.com>
2019-10-09 11:55:45 -0300

    first approach to state machine

--- pingbot.ino
+++ pingbot.ino
@@ -16,33 +16,23 @@ void setup() {
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+  digitalWrite(EnA,HIGH);
+  digitalWrite(EnB,HIGH);
+  set_speed(255, 255);
 }
 
 void loop() {
+  motors_stop();
+  delay(1000);
+  motors_left(10);
+  motors_stop();
+  delay(1000);
+  motors_right(10);
+}
 
-  digitalWrite(EnA,HIGH);
-  digitalWrite(EnB,HIGH);
-  analogWrite(EnA,255);
-  analogWrite(EnB,255);
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  delay(1000);
-  digitalWrite(EnA,LOW);
-  digitalWrite(EnB,LOW);
-  delay(1500);
-  /*
-  
-  motors_stop();
-  delay(1000);
-  motors_right();
-  delay(1000);
-  motors_stop();
-  delay(1000);
-  motors_left();
-  delay(1000);
-  */
+void set_speed(int v_a, int v_b) {
+  analogWrite(EnA, v_a);
+  analogWrite(EnB, v_b);
 }
 
 void motors_stop() {
@@ -50,6 +40,7 @@ void motors_stop() {
   digitalWrite(7, LOW);
   digitalWrite(8, LOW);
   digitalWrite(9, LOW);
+  delay(50);
 }
 
 void motors_backward() {
@@ -66,17 +57,26 @@ void motors_forward() {
   digitalWrite(9, LOW);
 }
 
-void motors_right() {
-  analogWrite(EnB,50);
-  digitalWrite(6, LOW);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
+void motors_right(int steps) {
+  for(int i = 0; i < steps; i++) {
+      digitalWrite(6, LOW);
+      digitalWrite(7, HIGH);
+      digitalWrite(8, LOW);
+      digitalWrite(9, HIGH);
+      delay(50);
+      motors_stop();
+  } 
+
 }
 
-void motors_left() {
-   digitalWrite(6, HIGH);
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
+void motors_left(int steps) {  
+  for(int i = 0; i < steps; i++) {
+      digitalWrite(6, HIGH);
+      digitalWrite(7, LOW);
+      digitalWrite(8, HIGH);
+      digitalWrite(9, LOW);
+      delay(50);
+      motors_stop();
+  }
+
 }
