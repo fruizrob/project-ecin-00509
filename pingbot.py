7b6a1f36562868ab270ee787c92710d242792db3
Felipe <fruiz.rob@gmail.com>
2020-04-06 21:32:36 -0400

    fix color detection and now connection between raspi-arduino works

--- pingbot.py
+++ pingbot.py
@@ -10,10 +10,12 @@ connection.flushInput()
 cap = cv2.VideoCapture(0)
 
 # celeste
-lowBlue = np.array([70, 50, 50], np.uint8)
-highBlue = np.array([100, 255, 255], np.uint8)
+lowBlue = np.array([80, 100, 50], np.uint8)
+highBlue = np.array([110, 255, 255], np.uint8)
 
+cont = 0
 while True:
+    cont += 1
     ret, frame = cap.read()
     if ret == True:
         # cambia formato
@@ -42,36 +44,40 @@ while True:
                 # 1298 = 22 cm (distancia camara a objeto) * 59 px (altura del objeto, se debe reemplazar).
                 cv2.putText(frame, "%.2fcm" % proximity, (frame.shape[1] - 300, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), 3)
                 cv2.drawContours(frame, [newContour], 0, (255, 0, 0), 3)
-    
-        if proximity == 0 and x == 0:
-            if(instruction != 0):
-                instruction = 0
+
+        if cont%10 == 0:
+            # if connection.in_wating:
+            #     print (connection.read())
+
+            if proximity == 0 and x == 0:
                 connection.write(bytes('0', 'UTF-8'))
-                print("scan")
-        else:
-            if x < 380 and x > 260 and proximity > 23:
-                if instruction != 1:
-                    instruction = 1
+                if(instruction != 0):
+                    instruction = 0
+                    print("scan")
+            else:
+                if x < 380 and x > 260 and proximity > 23:
                     connection.write(bytes('1', 'UTF-8'))
-                    print("move forward")
+                    if instruction != 1:
+                        instruction = 1
+                        print("move forward")
 
-            elif x > 380:
-                if instruction != 2:
-                    instruction = 2
+                elif x > 380:
                     connection.write(bytes('2', 'UTF-8'))
-                    print("move right")
+                    if instruction != 2:
+                        instruction = 2
+                        print("move right")
 
-            elif x < 260:
-                if instruction != 3:
-                    instruction = 3
+                elif x < 260:
                     connection.write(bytes('3', 'UTF-8'))
-                    print("move left")
+                    if instruction != 3:
+                        instruction = 3
+                        print("move left")
 
-            elif proximity < 23:
-                if instruction != 4:
-                    instruction = 4
+                elif proximity < 23:
                     connection.write(bytes('4', 'UTF-8'))
-                    print("collect")
+                    if instruction != 4:
+                        instruction = 4
+                        print("collect")
 
         cv2.imshow('pingbot', frame)
 
